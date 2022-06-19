952938e5fc56e3eaca7aff82d07050c182f89f25
Sharath <sharathwelcomesyouin@gmail.com>
2022-06-19 12:06:39 +0530

    learn react basics

---
+++ src/index.js
@@ -0,0 +1,38 @@
+// import the react and react dom libraries
+import React from "react";
+// import ReactDOM  from "react-dom";
+import ReactDOM  from "react-dom/client";
+
+// create a react Component
+function getButtonName(){
+    return "Click Here!"
+}
+
+const App = ()=>{
+    const name= {text: 'Click Me'};
+    return (
+        <div>
+            <label htmlFor="name">Enter name</label>      
+            <input id="name" type="text" />   
+            <button style={{backgroundColor:'green',color:' '}}>
+                {getButtonName()}
+            </button>
+            <button>
+                {name.text}
+            </button>
+        </div>
+    );
+}
+
+
+// take the react component and show it on the screen
+
+// ReactDOM.render(
+//     <App/>,
+//     document.getElementById("root")
+// );
+
+const root = ReactDOM.createRoot(document.getElementById('root'));
+root.render(  
+    <App />  
+);
